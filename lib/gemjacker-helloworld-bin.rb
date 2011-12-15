e53c56da5be8a57be7e28430572d481a7b522c73
John Van Enk <vanenkj@gmail.com>
2011-12-15 11:10:33 -0500

    Fleshed out a simple example of adding a binary search path.

--- lib/gemjacker-helloworld-bin.rb
+++ lib/gemjacker-helloworld-bin.rb
@@ -1,7 +1,15 @@
 require 'gemjacker'
 
-class HelloWorld < GemJacker
-  def hw_location
-    __FILE__
+module GemJacker
+  class HelloWorld
+    # Priority 1 so it's loaded over normal system paths.
+    def self.register(priority=1)
+      here = File.dirname(__FILE__)
+      there = "../files/bin"
+
+      search_path = File.expand_path(File.join(here, there))
+
+      GemJacker::ExecutablePaths.add_path(search_path)
+    end
   end
 end
